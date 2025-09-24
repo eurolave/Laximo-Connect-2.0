790733b5ec0b14f5e5a66a540d84d35b42d997f8
eurolave <eurolave@gmail.com>
2025-09-24 16:55:46 +0300

    Update LaximoClient.php

--- src/LaximoClient.php
+++ src/LaximoClient.php
@@ -1,8 +1,8 @@
 <?php
 namespace App;
 
-use Laximo\Guayaquil\ServiceOem;
-use Laximo\Guayaquil\ServiceAm;
+use GuayaquilLib\ServiceOem;
+use GuayaquilLib\ServiceAm;
 
 class LaximoClient {
     private ServiceOem $oem;
@@ -13,13 +13,11 @@ class LaximoClient {
         $this->am  = new ServiceAm($login, $password);
     }
 
-    /** Поиск авто по VIN */
     public function findByVin(string $vin): array {
         $res = $this->oem->findVehicleByVin($vin);
         return is_array($res) ? $res : [];
     }
 
-    /** Поиск по артикулу (Aftermarket / DOC) */
     public function findOem(string $article, ?string $brand=null): array {
         $res = $this->am->findOem($article, $brand ?? '');
         return is_array($res) ? $res : [];
